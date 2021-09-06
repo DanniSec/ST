6f92bae9f3a03831529d725adf18d33714c232af
SoudruhDanny <agarczechsparta@gmail.com>
2021-09-06 20:09:04 +0200

    black cock

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions
 from riotwatcher import LolWatcher, ApiError
+from bs4 import BeautifulSoup
 
 def get_prefix(bot, message):
     with open('data/prefixes.json', 'r') as f:
@@ -18,26 +19,15 @@ config = open('config.json')
 data = json.load(config)
 apikey = data['apikey']
 
+
 watcher = LolWatcher(apikey)
 token = data['token']
 
 config.close()
 
-server = "eune"  # input("Region: ")
-
 imgList = ['img/IRON.png', 'img/BRONZE.png', 'img/SILVER.png', 'img/GOLD.png', 'img/PLATINUM.png', 'img/DIAMOND.png',
            'img/MASTER.png', 'img/GRANDMASTER.png', 'img/CHALLENGER.png']
 
-if server == "eune":
-    region = "eun1"
-elif server == "euw":
-    region = "euw1"
-elif server == "NA":
-    region = "na1"
-elif server == "KR":
-    region = "kr"
-
-url = f"https://{region}.api.riotgames.com"
 
 @bot.event
 async def on_ready():
@@ -45,26 +35,50 @@ async def on_ready():
 
 
 @bot.command(name='profile')
-async def profile(message, *, value):
-    name_url = watcher.summoner.by_name(region, f'{value}')
+async def profile(ctx, message, *, value):
 
-    soloRank = watcher.league.by_summoner(region, name_url['id'])
+    rawValue = [x.strip() for x in value.split('-')]
+    value = rawValue[0]
+
+    try:
+        server = rawValue[1]
+    except IndexError:
+        server = "eune"
+
+
+    if server == "eune":
+        region = "eun1"
+    elif server == "euw":
+        region = "euw1"
+    elif server == "na":
+        region = "na1"
+    elif server == "kr":
+        region = "kr"
+
+
+    print(f"{region} {server}")
+
+    try:
+        name_url = watcher.summoner.by_name(region, f'{rawValue[0]}')
+        soloRank = watcher.league.by_summoner(region, name_url['id'])
+    except requests.exceptions.HTTPError:
+        ("ERROR: SUMMONER NOT FOUND")
 
     if (' ' in value) == True:
         opggValue = value.replace(" ", "+")
         opggUrl = f"https://eune.op.gg/summoner/userName={opggValue}"
     else:
-        opggUrl = f"https://eune.op.gg/summoner/userName={value}"
+        opggUrl = f"https://eune.op.gg/summoner/userName={rawValue[0]}"
 
     if (' ' in value) == True:
         mpValue = value.replace(" ", "%20")
-        mpUrl = f"https://www.masterypoints.com/image/profile/{mpValue}/{server}"
+        mpUrl = f"https://www.masterypoints.com/image/profile/{mpValue}/{server.lower()}"
     else:
-        mpUrl = f"https://www.masterypoints.com/image/profile/{value}/{server}"
+        mpUrl = f"https://www.masterypoints.com/image/profile/{rawValue[0]}/{server.lower()}"
 
     rank = soloRank[0]['tier']
     rank += '.png'
-    file = discord.File(f"img/{rank}", filename=f"{rank}")
+
 
     print(soloRank)
     slRank = "UNRANKED"
@@ -82,7 +96,7 @@ async def profile(message, *, value):
         else:
             fxRank = temp
 
-
+    file = discord.File(f"img/{slRank.split()[0]}.png", filename=f"{rank}")
     embed = discord.Embed(colour=discord.Colour.random())
 
     embed.add_field(name=f"LEVEL", value=f"{name_url['summonerLevel']}")
@@ -95,7 +109,7 @@ async def profile(message, *, value):
 
     embed.set_image(url=mpUrl)
 
-    embed.set_author(name=f"{value}",
+    embed.set_author(name=f"{rawValue[0]} {server.upper()}",
                      url=opggUrl,
                      icon_url=f"http://ddragon.leagueoflegends.com/cdn/11.17.1/img/profileicon/"
                               f"{name_url['profileIconId']}.png")
